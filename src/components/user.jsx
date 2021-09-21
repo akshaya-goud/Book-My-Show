13f6c66aad031b12bcb90ab29e356eff0956205d
akshaya <akshayagoud101@gmail.com>
2021-09-22 00:30:38 +0530

    my appproject setup

---
+++ src/components/user.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+class User extends React.Component {
+    constructor(props) {
+    super(props);
+    this.state ={
+        planet: "earth",
+    };
+    console.log("Hey I'm from Constructor");
+    }
+    
+    componentDidMount(){
+        this.setState({planet : "Jupiter"});
+    }
+
+
+   // static getDerivedStateFromProps(prop, state) {
+   //   console.log("I'm from get derived state from props");
+   // return {planet: "Jupiter"};
+   //}
+
+    shouldComponentUpdate(nextProp, nextState){
+        console.log("from shouldComponentUpdate")
+        console.log({
+            nextProp,
+            nextState,
+        });
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProp, prevState) {
+        console.log("from getSnapshotBeforeUpdate")
+        console.log({ prevProp, prevState});
+        return true;
+    }
+
+    componentDidUpdate(){
+        console.log(this.state);
+    }
+ 
+
+    render() {
+        console.log("Hey I'm from render()");
+        return(
+            <div>
+                <h1>{this.props.name}</h1>
+                <p>{this.props.description}</p>
+                <h4>{this.state.planet}</h4>
+            </div>
+        );
+    } 
+}
+
+export default User;
