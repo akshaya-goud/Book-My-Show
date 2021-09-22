a8f167cfa96b44ff9f4ed384a0fb8cc9c013f9b7
akshaya <akshayagoud101@gmail.com>
2021-09-22 22:28:39 +0530

    added HOC

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,54 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-class User extends React.Component {
-    constructor(props) {
-    super(props);
-    this.state ={
-        planet: "earth",
-    };
-    console.log("Hey I'm from Constructor");
-    }
-    
-    componentDidMount(){
-        this.setState({planet : "Jupiter"});
-    }
+const User = (props) => {
+    const[planet, setPlanet]  = useState("earth");
 
+//componetDidMount
+    useEffect(() => {
 
-   // static getDerivedStateFromProps(prop, state) {
-   //   console.log("I'm from get derived state from props");
-   // return {planet: "Jupiter"};
-   //}
+        console.log("component mounting");
+        //componentWillUnmount
+        return console.log("Bye-Bye");
+    } , []);
 
-    shouldComponentUpdate(nextProp, nextState){
-        console.log("from shouldComponentUpdate")
-        console.log({
-            nextProp,
-            nextState,
-        });
-        return true;
-    }
+//componentDidUpdate
+    useEffect(() => {
+        console.log("planet changed");
+    }, [planet]);
 
-    getSnapshotBeforeUpdate(prevProp, prevState) {
-        console.log("from getSnapshotBeforeUpdate")
-        console.log({ prevProp, prevState});
-        return true;
-    }
+    return(
+        <div>
+            <h1>{props.name}</h1>
+            <p>{props.description}</p>
+            <button onClick={() => setPlanet("pluto")}>
+                {planet}
+            </button>
+        </div>
+    );
+};
 
-    componentDidUpdate(){
-        console.log(this.state);
-    }
- 
-
-    render() {
-        console.log("Hey I'm from render()");
-        return(
-            <div>
-                <h1>{this.props.name}</h1>
-                <p>{this.props.description}</p>
-                <h4>{this.state.planet}</h4>
-            </div>
-        );
-    } 
-}
 
 export default User;
---
+++ src/planning.js
@@ -0,0 +1,20 @@
+//book-my-show planning
+
+// -> we need a reusable carousel
+// -> hero carousel
+// -> common carousel
+// -> cast and crew carousel
+
+//Pages
+
+// -> home page
+// -> movie page
+// -> category page
+// -> Authentication page
+// -> Razorpay payment gateway
+
+//plan????
+// layouts
+// Routes
+// Pages
+// Components
